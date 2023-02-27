40eacc69965a75b809358b7b4ba72334c7d243b3
guille garcia <ggarciacardiel@gmail.com>
2023-02-27 14:21:18 +0100

    it6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 import {usersArray as users} from './data/users';
 import {cardsArray as cards} from './data/cards'
 
@@ -16,18 +17,25 @@ function App() {
         return <IdCard key={elem._id} user={elem} />
       })}
     <h1>Greetings</h1>  
-    <Greetings lang="de">Ludwig</Greetings>
-    <Greetings lang="fr">François</Greetings>
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
     <h1>Random</h1>     
-    <Random min={1} max={6}/>
-    <Random min={1} max={100}/>
+      <Random min={1} max={6}/>
+      <Random min={1} max={100}/>
     <h1>BoxColor</h1>    
-    <BoxColor r={255} g={0} b={0} />
-    <BoxColor r={128} g={255} b={0} />
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     <h1>CreditCard</h1>
-    {cards.map(elem => {
-        return <CreditCard key={elem._id} card={elem} />
-      })}
+      {cards.map(elem => {
+          return <CreditCard key={elem._id} card={elem} />
+        })}
+    <h1>Rating</h1>
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>  
     </div>
   );
 }
---
+++ src/components/Rating.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import filedStar from '../assets/images/filedStar.png'
+import emptyStar from '../assets/images/emptyStar.png'
+
+const Rating = (props) => {
+  const {children} = props;
+  const rating = [];
+  const roundedRating = Math.round(children);
+  const style = {
+    width: '15px',
+    height: '15px'
+  };
+  for (let i = 0; i<=4; i++) {
+    /* loops out of 4 to print 5 stars */
+    rating.push(
+      i < roundedRating ? 
+      <img style={style} key={i} src={filedStar} alt='filled' /> : 
+      <img style={style} key={i} src={emptyStar} alt='empty' />
+    )
+  }
+  return (
+    <div>{rating}</div>
+  )
+}
+export default Rating;
