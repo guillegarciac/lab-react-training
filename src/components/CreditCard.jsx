d228c2c6aba01cde971c4ed5da9512e2e92ffac6
guille garcia <ggarciacardiel@gmail.com>
2023-02-27 16:05:49 +0100

    css it5

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,22 +1,30 @@
 import React from "react";
+import visa from "../assets/images/visa.png"
+import mastercard from "../assets/images/mastercard.png"
 
 const CreditCard = (props) => {
   const {card: {type, number, expirationMonth, expirationYear, bank, owner, bgColor, color}} = props;
-  const background = {
-    backgroundColor: `${bgColor}`
-  }
-  const cardColor = {
-    cColor: `${color}`
+  
+  let imageCC = "";
+  switch(type) {
+    case "Visa": imageCC = visa; break;
+    case "Master Card": imageCC = mastercard; break;
+    default: imageCC = "";
+  };
+  const hashNumber = "**** **** **** " + number.slice(12, 16);
+
+  const creditCardStyle = {
+    backgroundColor: bgColor,
+    color: color,
   }
 
   return (
-    <div className="credit-card" style={background}>
-			<p>{type}</p>
-			<p>{number}</p>
-			<div className="card-data" style={cardColor}>
-				<p>Expires: {expirationMonth}/{expirationYear}</p>
-				<p>{bank}</p>
-				<p>{owner}</p>
+    <div className="credit-card" style={creditCardStyle}>
+			<img width="40px" src={imageCC} alt="logo" />
+      <h2>{hashNumber}</h2>
+			<div className="card-data">
+				<p>Expires: {expirationMonth}/{expirationYear} {bank}</p>
+        <p>{owner}</p>
 			</div>
 		</div>
   )
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -4,9 +4,7 @@ const Random = (props) => {
 	const {min, max} = props;
   const getRandom = Math.floor(Math.random() * (max - min + 1)) + min;
   return (
-    <p className="random">
-      Random value between {min} and {max} => {getRandom}
-    </p>
+    <p className="random">Random value between {min} and {max} = {getRandom}</p>
     )
   }
 
