912ff7b254ea3f0d0b491b7fefa030f004729e05
guille garcia <ggarciacardiel@gmail.com>
2023-03-06 18:42:12 +0100

    it15 advanced filtering countries

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -3,12 +3,47 @@ import React, {useState} from 'react'
 
 export default function Facebook() {
   //setProfiles seems as is not being used in the function because the component is not calling it. However, when the component is first mounted, the useState hook initializes the profiles state to the profilesData array passed as an argument and this is achieved by destructuring the returned array of [profiles, setProfiles], and assigning the first element (profiles) to the initial state value, and the second element (setProfiles) to the state updater function.
-  
+
   const [profiles, setProfiles] = useState(profilesData);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  //Array of countries from the profiles data
+  const countries = [...new Set(profiles.map((profile) => profile.country))];
+
+  //Creates filteredProfiles to filters profiles by selected country (based on countries above), or show all profiles if none is selected (: profiles)
+  const filteredProfiles = selectedCountry ? profiles.filter((profile) => profile.country === selectedCountry) : profiles;
+
+  //Handles country button clicks
+  const handleCountryClick = (country) => {
+    setSelectedCountry(country);
+  };
+
+  //Handles button click to select All countries => selectedCountry = null
+  const handleAllCountriesClick = () => {
+    setSelectedCountry(null)
+  }
 
   return (
     <div>
-      {profiles.map((profile, index) => (
+      <div>
+        <button 
+          onClick={handleAllCountriesClick} 
+          style={{
+            backgroundColor: selectedCountry === null ? "#43A5BE" : "transparent", 
+            color: selectedCountry === null ? "white" : "black"}}>All</button>
+        {countries.map((country, index) => (
+        <button 
+          key={index}
+          onClick={() => handleCountryClick(country)}
+          style={{
+            backgroundColor: selectedCountry === country ? "#43A5BE" : "transparent",
+            color: selectedCountry === country ? "white" : "black"}}>
+          {country}
+        </button>
+      ))}
+      </div>
+    <div>
+      {filteredProfiles.map((profile, index) => (
         <div className="idCard" key={index}>
           <img src={profile.img} alt={profile.firstName + " " + profile.lastName} />
           <div className="cardIDetails">
@@ -20,5 +55,6 @@ export default function Facebook() {
         </div>
       ))}
     </div>
+  </div>
   );
 }
